48f25d75ce908eda856135f916e2f2a7a8bd7da4
Qu1ckIy <60589133+Soy1556@users.noreply.github.com>
2022-05-29 14:02:27 -0500

    undo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = process.env.token
+const { token } = require("config.json")
 
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS);
