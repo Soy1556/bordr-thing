9ac6946e1704540fc7b45bbbab9efce03155494e
Qu1ckIy <60589133+Soy1556@users.noreply.github.com>
2022-05-28 23:08:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS); //find this intent, it's causing the error.
+
+const client = new Client({ intents: myIntents });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+
+client.login(token);
