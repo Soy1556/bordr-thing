c9505e41e72bdeac92e2fc7bcc1a60155d0c3d7e
Qu1ckIy <60589133+Soy1556@users.noreply.github.com>
2022-05-29 13:59:49 -0500

    add process token

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = process.env.token
 
 const myIntents = new Intents();
-myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS); //find this intent, it's causing the error.
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS);
 
 const client = new Client({ intents: myIntents });
 
