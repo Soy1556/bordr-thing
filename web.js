c0f0f37b4549512d922cef5755981e565ef18e64
Qu1ckIy <60589133+Soy1556@users.noreply.github.com>
2022-06-01 14:46:46 -0500

    Create web.js

---
+++ web.js
@@ -0,0 +1,16 @@
+
+// Made by my friend, KingCh1ll. //
+// Last Edited: 2/18/2021 //
+// website.js //
+
+// Librarys //
+const express = require("express");
+const path = require("path");
+
+// App //
+const app = express();
+
+// Code //
+console.log("-------- Website --------");
+app.use("*", (req, res) => res.status(404).send({ status: 404, message: "This API route does not exist." }));
+app.listen(process.env.PORT || 3000, () => console.log("💻 | Server listening to port 3000."));
